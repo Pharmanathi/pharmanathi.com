804d76664a17825fa240babbad895c1f9aed1995
Nehemie BALUKIDI <alfred.nehemie@gmail.com>
2024-06-17 12:19:21 +0200

    feat(Adminsite): intiail version for MHP profile (in)validation

--- pharmanathi.com/pharmanathi_backend/adminsite/tests.py
+++ pharmanathi.com/pharmanathi_backend/adminsite/tests.py
@@ -1,3 +1,10 @@
-from django.test import TestCase
+import pytest
 
-# Create your tests here.
+pytestmark = pytest.mark.django_db
+
+
+def test_home_stats(staff_web_client, unverified_mhp_client):
+    res = staff_web_client.get("/custom-admin/")
+    print(dir(res), res.context.get("stats"))
+    assert res.status_code == 200
+    assert res.context.get("stats").get("unverifed_mhps") == 1
