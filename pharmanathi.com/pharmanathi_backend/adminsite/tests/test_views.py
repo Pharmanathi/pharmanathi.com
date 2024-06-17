fcf67f005bd3b67636931e21654a55ea5319775d
Nehemie BALUKIDI <alfred.nehemie@gmail.com>
2024-06-17 13:27:36 +0200

    test: ogranise tests

--- pharmanathi.com/pharmanathi_backend/adminsite/tests/test_views.py
+++ pharmanathi.com/pharmanathi_backend/adminsite/tests/test_views.py
@@ -3,6 +3,11 @@ import pytest
 pytestmark = pytest.mark.django_db
 
 
+def test_admisite_redirect_to_login_if_not_authed(web_client):
+    res = web_client.get("/custom-admin/MHPs/unverified/")
+    assert res.status_code == 302
+
+
 def test_home_stats(staff_web_client, unverified_mhp_client):
     res = staff_web_client.get("/custom-admin/")
     print(dir(res), res.context.get("stats"))
