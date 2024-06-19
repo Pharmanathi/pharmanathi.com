5740ad6f6720dff7acb59471109869ecb2f40543
Néhémie <Alfred.nehemie@gmail.com>
2024-06-19 04:29:47 +0200

    feat: notify MHPs and admins via email (#124)
    * feat: notify via email MHPs and admins
    
    Notify admins and MHPs on issues around MHP profile validation and invalidation
    
    * feat: notify on new signin
    
    * chore: re-order EMAIL section's configuration
    
    * test: add more tests
    
    * feat: add mandatory reason when invalidating profiles (#126)
    
    * Invalidating MP profiles is now a post request since requiring supplying the reason(s) for invalidation. Hence, invalidating prompts for the mandatory reason(s)
    
    * Provide UI for viewing and resolving Invalidation Messages
    
    * Update _empty.html to include block added to base.html
    
    * fix: missing trailing slash for requests to /api/doctors/

--- pharmanathi.com/pharmanathi_backend/adminsite/static/adminsite-2/js/custom.js
+++ pharmanathi.com/pharmanathi_backend/adminsite/static/adminsite-2/js/custom.js
@@ -16,7 +16,7 @@ function viewUser(id) {
 }
 
 function markMHPAsVerified(mhpId) {
-    fetch(`/custom-admin/MHPs/actions/${mhpId}/mark-verified/`, { headers: GLOBAL_CONF.defaultHTTPRequestHeader() })
+    fetch(`/custom-admin/MHPs/${mhpId}/mark-verified/`, { headers: GLOBAL_CONF.defaultHTTPRequestHeader() })
         .then(res => {
             if (res.ok) location.reload()
             else return res.json()
@@ -26,8 +26,15 @@ function markMHPAsVerified(mhpId) {
         })
 }
 
-function invalidateMHPProfile(mhpId) {
-    fetch(`/custom-admin/MHPs/actions/${mhpId}/invalidate/`, { headers: GLOBAL_CONF.defaultHTTPRequestHeader() })
+function invalidateMHPProfile(mhpId, reason) {
+    return fetch(`/custom-admin/MHPs/${mhpId}/invalidate/`, {
+        method: "post",
+        headers: { 
+            ...GLOBAL_CONF.defaultHTTPRequestHeader(),
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify({reason}),
+    })
         .then(res => {
             if (res.ok) location.reload()
             else return res.json()
@@ -69,11 +76,38 @@ function notify(message, title = "", type = "primary") {
     toastElement.show()
 }
 
+function resolveInvalidationReason(irId){
+    return fetch(`/custom-admin/IRs/${irId}/resolve/`, {
+        headers: { 
+            ...GLOBAL_CONF.defaultHTTPRequestHeader()
+        }
+    })
+        .then(res => {
+            if (res.ok) location.reload()
+            else return res.json()
+        })
+        .then(data => {
+            notify(data.detail, `IR resolution error`, "danger")
+        })
+}
+
 /********************************************  Event Listeners  ***********************ß*********************/
 document.querySelector("#btn-logout").addEventListener("click", logout)
-document.querySelector("#btn-mhp-validate-mhp-profile").addEventListener("click", (e)=>{
+document.querySelector("#btn-mhp-validate-mhp-profile").addEventListener("click", (e) => {
     markMHPAsVerified(e.target.dataset.mhpId)
 })
-document.querySelector("#btn-mhp-invalidate-mhp-profile").addEventListener("click", (e)=>{
-    invalidateMHPProfile(e.target.dataset.mhpId)
+document.querySelector("#btn-mhp-invalidate-mhp-profile").addEventListener("click", (e) => {
+    (new bootstrap.Modal("#modal-profile-invaliadtion")).show()
+
+})
+
+document.getElementById("btn-confirm-mhp-invalidate-mhp-profile").addEventListener("click", e => {
+    const form = new FormData(document.querySelector("[name='frm-invalidate-mhp']"))
+    const mhpId = form.get("mhp")
+    const reason_text = encodeURIComponent(form.get("reason"))
+    invalidateMHPProfile(mhpId, reason_text)
+})
+
+$("body").on("click", ".btn-ir-resolver", e => {
+    resolveInvalidationReason(e.target.dataset.irId)
 })
