4dc1e45851cf1bdf27521fab9db3bfeea1236e0d
FLAVIYO <shongwe1408@gmail.com>
2024-07-17 17:15:32 +0200

    use ValueNotifier in UserBloc

--- mobile-mhp/lib/blocs/appointment_bloc.dart
+++ mobile-mhp/lib/blocs/appointment_bloc.dart
@@ -1,22 +1,30 @@
 import 'package:flutter/material.dart';
+import 'package:sentry_flutter/sentry_flutter.dart';
 import '../models/appointment.dart';
 import '../repositories/appointment_repository.dart';
 
 class AppointmentBloc {
   final AppointmentRepository _appointmentRepository;
-  final ValueNotifier<List<Appointment>?> _appointmentsNotifier = ValueNotifier<List<Appointment>?>(null);
+  final ValueNotifier<List<Appointment>?> _appointmentsNotifier =
+      ValueNotifier<List<Appointment>?>(null);
 
-  ValueNotifier<List<Appointment>?> get appointmentsNotifier => _appointmentsNotifier;
+  ValueNotifier<List<Appointment>?> get appointmentsNotifier =>
+      _appointmentsNotifier;
 
   AppointmentBloc(this._appointmentRepository);
 
   Future<void> fetchAppointments(BuildContext context) async {
     try {
-      final appointments = await _appointmentRepository.fetchAppointments(context);
+      final appointments =
+          await _appointmentRepository.fetchAppointments(context);
       _appointmentsNotifier.value = appointments;
-    } catch (e) {
+    } catch (e, stackTrace) {
       // Handle error
       _appointmentsNotifier.value = null;
+      await Sentry.captureException(
+        e,
+        stackTrace: stackTrace,
+      );
     }
   }
 
--- mobile-mhp/lib/blocs/user_bloc.dart
+++ mobile-mhp/lib/blocs/user_bloc.dart
@@ -1,23 +1,33 @@
 import 'dart:async';
 import 'package:flutter/material.dart';
+import 'package:sentry_flutter/sentry_flutter.dart';
 import '../models/user.dart';
 import '../repositories/user_repository.dart';
 
+
 class UserBloc {
   final UserRepository _userRepository;
-  final StreamController<User?> _userController = StreamController<User?>();
+  final ValueNotifier<User?> _userNotifier = ValueNotifier<User?>(null);
 
-  Stream<User?> get user => _userController.stream;
+  ValueNotifier<User?> get userNotifier => _userNotifier;
 
   UserBloc(this._userRepository);
 
-  fetchUserData(BuildContext context) async {
-    final user = await _userRepository.fetchUserData(context);
-    _userController.sink.add(user);
+  Future<void> fetchUserData(BuildContext context) async {
+    try {
+      final user = await _userRepository.fetchUserData(context);
+      _userNotifier.value = user;
+    } catch (e, stackTrace) {
+      // Handle error
+      _userNotifier.value = null;
+      await Sentry.captureException(
+        e,
+        stackTrace: stackTrace,
+      );
+    }
   }
 
   void dispose() {
-    _userController.close();
+    _userNotifier.dispose();
   }
 }
-
