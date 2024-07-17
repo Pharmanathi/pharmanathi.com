c8a41b48011d1e21161c166e9e6cfdf5d1efe5c6
FLAVIYO <shongwe1408@gmail.com>
2024-07-17 11:13:54 +0200

    update: suggested changes

--- mobile-mhp/lib/models/user.dart
+++ mobile-mhp/lib/models/user.dart
@@ -5,12 +5,7 @@ class User {
   bool isDoctor;
   DoctorProfile? doctorProfile;
   DateTime? lastLogin;
-  bool isSuperuser;
-  bool isStaff;
   bool isActive;
-  DateTime? dateJoined;
-  DateTime? dateCreated;
-  DateTime? dateModified;
   String firstName;
   String lastName;
   String email;
@@ -26,12 +21,7 @@ class User {
     required this.isDoctor,
     this.doctorProfile,
     this.lastLogin,
-    required this.isSuperuser,
-    required this.isStaff,
     required this.isActive,
-    this.dateJoined,
-    this.dateCreated,
-    this.dateModified,
     required this.firstName,
     required this.lastName,
     required this.email,
@@ -49,12 +39,7 @@ class User {
       isDoctor: json['is_doctor'],
       doctorProfile: json['doctor_profile'] ,
       lastLogin: json['last_login'] != null ? DateTime.parse(json['last_login']) : null,
-      isSuperuser: json['is_superuser'],
-      isStaff: json['is_staff'],
       isActive: json['is_active'],
-      dateJoined: json['date_joined'] != null ? DateTime.parse(json['date_joined']) : null,
-      dateCreated: json['date_created'] != null ? DateTime.parse(json['date_created']) : null,
-      dateModified: json['date_modified'] != null ? DateTime.parse(json['date_modified']) : null,
       firstName: json['first_name'],
       lastName: json['last_name'],
       email: json['email'],
