3819673d7e217d89899ba5abb75ee4c8724cfb57
Nehemie BALUKIDI <alfred.nehemie@gmail.com>
2024-08-09 01:10:45 +0200

    fix: deeplink handler should carry the received arguments over

--- pharmanathi.com/pharmanathi_backend/utils/mobile_client_handlers.py
+++ pharmanathi.com/pharmanathi_backend/utils/mobile_client_handlers.py
@@ -1,5 +1,9 @@
+import logging
+
 from django.http import HttpResponseRedirect
 from django.shortcuts import redirect, resolve_url
+
+logger = logging.getLogger(__file__)
 
 
 def handle_ios_app_association(request):
@@ -9,10 +13,13 @@ def handle_ios_app_association(request):
 def handle_deeplink_redirect(request):
     HttpResponseRedirect.allowed_schemes.append("news")
     full_path = request.get_full_path()
-    args = full_path[full_path.index("?") + 1 :].split("=")
-    screen = args[args.index("screen") + 1]
+    args = full_path[full_path.index("?") :]
+
+    deeplink = f"unilinks://pharmanathi.com/{args}"
+    logger.info(f"Deeplink redirect to {deeplink} from {full_path}")
 
     class UnilinkRedirect(HttpResponseRedirect):
         allowed_schemes = ["unilinks"]
 
-    return UnilinkRedirect(resolve_url(f"unilinks://pharmanathi.com{screen}"))
+    logger.info(f"Deeplink redirect to {deeplink} from {full_path}")
+    return UnilinkRedirect(resolve_url(deeplink))
