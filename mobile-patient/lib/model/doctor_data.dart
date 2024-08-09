6926ce594fdb8804c476fe84720cb5719fceed94
Nehemie BALUKIDI <alfred.nehemie@gmail.com>
2024-08-09 04:42:24 +0200

    fix(patient): handling of appointment's type and other
    - fix handling of appointment type
    - renamed string representation of appointment type to `appointmentTypeRrep` for Representaion
    - refactor handling of specialities

--- mobile-patient/lib/model/doctor_data.dart
+++ mobile-patient/lib/model/doctor_data.dart
@@ -5,21 +5,21 @@ class Doctor {
   final bool isVerified;
   final String imageURL;
   final String doctorName;
-  final List<Speciality> specialities;
-  final List<int> practiceLocations;
+  final List<String> specialities;
+  // final List<int> practiceLocations;
   final int id;
   final bool hasConsultedBefore;
-  final List<AppointmentType> appointmentTypes;
+  // final List<AppointmentType> appointmentTypes;
 
   Doctor({
     required this.isVerified,
     required this.imageURL,
     required this.doctorName,
     required this.specialities,
-    required this.practiceLocations,
+    // required this.practiceLocations,
     required this.id,
     required this.hasConsultedBefore,
-    required this.appointmentTypes,
+    // required this.appointmentTypes,
   });
 
   factory Doctor.fromJson(Map<String, dynamic> json) {
@@ -31,29 +31,18 @@ class Doctor {
       doctorName: doctorName,
       isVerified: json['is_verified'],
       imageURL: json['imageURL'] ?? '',
-      specialities: (json['specialities'] as List<dynamic>)
-          .map((e) => Speciality.fromJson(e))
-          .toList(),
-      practiceLocations: List<int>.from(json['practicelocations']),
+      specialities: json['specialities'].cast<String>(),
+      // practiceLocations: List<int>.from(json['practicelocations']),
       id: json['id'],
       hasConsultedBefore: json['has_consulted_before'],
-      appointmentTypes: (json['appointment_types'] as List<dynamic>)
-          .map((e) => AppointmentType.fromJson(e))
-          .toList(),
+      // appointmentTypes: (json['appointment_types'] as List<dynamic>)
+      //     .map((e) => AppointmentType.fromJson(e))
+      //     .toList(),
     );
-  }
-
-  //* Returns the name of the first specialty, or an empty string if there are none
-  String getFirstSpecialityName() {
-    if (specialities.isNotEmpty) {
-      final name = specialities.first.name;
-      return name.length > 30 ? name.substring(0, 30) + '...' : name;
-    }
-    return ''; 
   }
 
   //* Returns all specialty names, each on a new line
   String getAllSpecialityNames() {
-    return specialities.map((speciality) => speciality.name).join('\n');
+    return specialities.join('\n');
   }
 }
