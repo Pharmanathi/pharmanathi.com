2e07df971e49d4d04b3e57c474f3ec19b64f5395
Nehemie BALUKIDI <alfred.nehemie@gmail.com>
2024-08-15 13:49:46 +0200

    fix(python, payments): handle cases where processing payment callback endpoint does not find a matching payment

--- pharmanathi.com/pharmanathi_backend/payments/views.py
+++ pharmanathi.com/pharmanathi_backend/payments/views.py
@@ -5,6 +5,7 @@ from django.shortcuts import HttpResponse
 from django.views.decorators.csrf import csrf_exempt
 from django.views.decorators.http import require_POST
 
+from .models import Payment
 from .providers.provider import get_provider
 
 logger = logging.getLogger(__file__)
@@ -14,7 +15,12 @@ logger = logging.getLogger(__file__)
 @require_POST
 def cb(request, provider):
     cb_request_data = json.loads(request.body)
-    logger.info(f"Got call from {provider} with body {cb_request_data}")
     provider_klass = get_provider(provider)  # We're hopping that Sentry catches any miss
-    provider_klass.process_payment(cb_request_data)
+    try:
+        provider_klass.process_payment(cb_request_data)
+    except Payment.DoesNotExist as e:
+        print(dir(e), e)
+        logger.warning(f"No {provider} payment satisifies callback with info {cb_request_data}")
+    except Exception as e:
+        raise e
     return HttpResponse("OK", content_type="text/plain")
