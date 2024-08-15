a31ad667a537fdad6249d49eb4f615f59491aced
Nehemie BALUKIDI <alfred.nehemie@gmail.com>
2024-08-15 13:57:13 +0200

    fix(patient): hitting /appointments displays an error if no appointments

--- mobile-patient/lib/blocs/appointment_bloc.dart
+++ mobile-patient/lib/blocs/appointment_bloc.dart
@@ -58,7 +58,7 @@ class AppointmentBloc {
   void _showSuccessMessage(BuildContext context) {
     ScaffoldMessenger.of(context).showSnackBar(
       const SnackBar(
-        content: Text('your appointment has been booked successfully!'),
+        content: Text('Your appointment has been booked successfully!'),
         duration: Duration(seconds: 5),
         behavior: SnackBarBehavior.floating,
         backgroundColor: Colors.green,
