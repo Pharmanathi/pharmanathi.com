5cff23460df27badb164db976ea298b07257fb91
Nehemie BALUKIDI <alfred.nehemie@gmail.com>
2024-08-16 21:06:38 +0200

    chore,ci(verifi): add test resources

---
+++ pharmanathi.com/verifi-standalone/sudos/hpcsa/record_files/iregister_details.3945e3ccefa0bd151ced1feb75059e2d.js
@@ -0,0 +1,2 @@
+
+RightNow.namespace('Custom.Widgets.searchForm.iRegisterDetails');Custom.Widgets.searchForm.iRegisterDetails=RightNow.Widgets.extend({constructor:function(){},methodName:function(){}});
