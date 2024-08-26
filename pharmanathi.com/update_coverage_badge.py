bb19bf53e97830f401b6f31c4971eb20024ba004
Nehemie BALUKIDI <alfred.nehemie@gmail.com>
2024-08-26 10:18:17 +0200

    ci(verifi, django): update test coverage updater script

--- pharmanathi.com/update_coverage_badge.py
+++ pharmanathi.com/update_coverage_badge.py
@@ -1,9 +1,15 @@
 # TODO(nehemie): replace this whole script with `sed`
+import argparse
 import sys
 from typing import Union
 
 PATH_MAIN_README = "../README.md"
-COVERAGE_LINE_START = "![coverage]"
+
+parser = argparse.ArgumentParser()
+parser.add_argument("-p", "--percentage", required=True)
+parser.add_argument(
+    "--starting-with", help="The text against which to test. Any line starting this way will be updated", required=True
+)
 
 
 def find_line_starting_with(phrase: str, lines) -> int | None:
@@ -29,16 +35,16 @@ def sanitize(arg: str) -> str:
 
 
 if __name__ == "__main__":
-    assert len(sys.argv) > 1, "Please supply coverage percentage"
-    coverage_percentage = sanitize(sys.argv[1])
+    args = parser.parse_args()
+    coverage_percentage = sanitize(args.percentage)
     with open(PATH_MAIN_README, "r+") as f:
         lines = f.readlines()
-        line_number = find_line_starting_with(COVERAGE_LINE_START, lines)
+        line_number = find_line_starting_with(args.starting_with, lines)
         if line_number:
             f.seek(0)
-            lines[
-                line_number
-            ] = f"![coverage](https://img.shields.io/badge/coverage-{coverage_percentage}%25-{get_color_name(int(coverage_percentage))})\n"
+            lines[line_number] = (
+                f"{args.starting_with}(https://img.shields.io/badge/coverage-{coverage_percentage}%25-{get_color_name(int(coverage_percentage))})\n"
+            )
             f.writelines(lines)
         else:
-            raise ValueError(f"Could not find line starting with `{COVERAGE_LINE_START}` in {PATH_MAIN_README}")
+            raise ValueError(f"Could not find line starting with `{args.starting_with}` in {PATH_MAIN_README}")
