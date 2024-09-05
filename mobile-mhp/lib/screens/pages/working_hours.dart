e868b5e2f5ff57ec10a1243e815ddd61944c99aa
Nehemie BALUKIDI <alfred.nehemie@gmail.com>
2024-09-05 15:31:39 +0200

    fix(mp): managing working hours

--- mobile-mhp/lib/screens/pages/working_hours.dart
+++ mobile-mhp/lib/screens/pages/working_hours.dart
@@ -22,7 +22,7 @@ class _WorkingHoursState extends State<WorkingHours> {
   final GlobalKey<ScaffoldState> _scaffoldKey =
       GlobalKey<ScaffoldState>(); // GlobalKey for the Scaffold
 
-  final Map<String, List> schedule = {
+  Map<String, List> schedule = {
     "1": [],
     "2": [],
     "3": [],
@@ -32,63 +32,118 @@ class _WorkingHoursState extends State<WorkingHours> {
     "7": [],
   };
 
+List<Map<String, dynamic>> realSchedule = [
+  {"dayStr": "Mon", "schedule": <List<TimeOfDay>>[]},
+  {"dayStr": "Tue", "schedule": <List<TimeOfDay>>[]},
+  {"dayStr": "Wed", "schedule": <List<TimeOfDay>>[]},
+  {"dayStr": "Thu", "schedule": <List<TimeOfDay>>[]},
+  {"dayStr": "Fri", "schedule": <List<TimeOfDay>>[]},
+  {"dayStr": "Sat", "schedule": <List<TimeOfDay>>[]},
+  {"dayStr": "Sun", "schedule": <List<TimeOfDay>>[]}
+];
+
   @override
   void initState() {
     super.initState();
-    //* Initialize the list with one WorkingHoursInput widget for each day
-    dayInputs = days.map((day) => _buildWorkingHoursInput(day, days)).toList();
-
-    //* Fetch schedule from API when the app starts
+    // (() async =>{
+    // //* Fetch schedule from API when the app starts
+    // await _fetchScheduleFromApi()
+    // this._buildWorkingHoursInput(day, days)
+    // })();
     _fetchScheduleFromApi();
+    // //* Initialize the list with one WorkingHoursInput widget for each day
+    // dayInputs = days.map((day) => _buildWorkingHoursInput(day, days)).toList();
   }
 
   //* Method to build WorkingHoursInput widget for a day
-  Widget _buildWorkingHoursInput(String day, List<String> days) {
+  // Widget _buildWorkingHoursInput(String day, List<String> days) {
+  Widget _buildWorkingHoursInput(int weekDayIndex, Map weekDay) {
     return WorkingHoursInput(
-      day: day,
-      onTimeChanged: (List<String> value) {
+      day: weekDay['dayStr'],
+      onTimeChanged: (List<dynamic> value) {
+        log.i("Fucing value $value");
         setState(() {
-          final List timeslots = _updateWorkingHours.call();
-          if (timeslots.isNotEmpty) {
-            String dayString = timeslots[0]["day"];
-            int dayInt = days.indexOf(dayString) + 1;
-            schedule[dayInt.toString()] = timeslots
-                .map((ts) => [ts["start_time"], ts["end_time"]])
-                .toList();
-          }
+          // final List timeslots = _updateWorkingHours.call();
+          // if (timeslots.isNotEmpty) {
+          //   // String dayString = timeslots[0]["day"];
+          //   // int dayInt = weekDayIndex;
+          //   // schedule[weekDayIndex.toString()] = timeslots
+          //   //     .map((ts) => [ts["start_time"], ts["end_time"]])
+          //   //     .toList();
+          // if(realSchedule[weekDayIndex]['schedule'].isEmpty) realSchedule[weekDayIndex]['schedule'] = <TimeOfDay>[];
+          if(realSchedule[weekDayIndex]['schedule'].length <= value[0]) realSchedule[weekDayIndex]['schedule'].add(<TimeOfDay>[]);
+            realSchedule[weekDayIndex]['schedule'][value[0]] = <TimeOfDay>[value[1], value[2]];
+          //   print("$timeslots Fuuuuuck --> $weekDayIndex ${realSchedule[weekDayIndex]['schedule']}");
+          // }
         });
-        log.d(day);
-        log.d(schedule);
+        log.d("Got -->>  $weekDayIndex $weekDay");
+        log.d(" Fake schedule $schedule");
+        log.d("Real Schedule $realSchedule");
+        log.i("Hello");
       },
       builder: (BuildContext context, Function() getTimeslots) {
         //* Assign the function to _updateWorkingHours
         _updateWorkingHours = () => getTimeslots();
       },
       days: days, //* Pass the 'days' parameter here
+      daySchedule: weekDay["schedule"],
+      // onNewSlot: (){
+      //   // Add a newly added slot to the schedule
+      // },
     );
   }
 
   //* Method to add a new WorkingHoursInput widget
-  void addDayInput() {
-    setState(() {
-      dayInputs.add(_buildWorkingHoursInput('New Day', days));
-    });
-  }
+  // void addDayInput() {
+  //   setState(() {
+  //     dayInputs.add(_buildWorkingHoursInput(0, days));
+  //   });
+  // }
 
   Future<void> _fetchScheduleFromApi() async {
     //* Call the fetchScheduleFromApi method
     Map<String, List> fetchedSchedule =
         await WorkingHourApiService.fetchScheduleFromApi(context);
 
+    
+    TimeOfDay buildTOD(String timeString){
+        List<int> parts = timeString.split(':').map(int.parse).toList();
+        return TimeOfDay(hour: parts[0], minute: parts[1]);
+      }
+
+
+    log.i('Sechdule was $schedule, updating it to $fetchedSchedule');
     setState(() {
-      schedule.addAll(fetchedSchedule);
+      // schedule.addAll(fetchedSchedule);
+      fetchedSchedule.forEach((index, schdl){
+        realSchedule[int.parse(index) - 1]["schedule"].clear();
+        for(int i=0;  i < schdl.length; i++){ 
+        var [start, end] = schdl[i].split(',');
+          realSchedule[int.parse(index) - 1]["schedule"].add([buildTOD(start), buildTOD(end)]);
+        }
+        
+      });
     });
-    log.i(fetchedSchedule);
   }
 
   //* Method to send schedule to API
   void _sendScheduleToApi() {
-    WorkingHourApiService.sendSchedule(schedule, context, onSuccess: () {
+    log.i("to convert >>>>> $realSchedule");
+    List<Map> convertedRealSchedule = [];
+    for(int i = 0; i < realSchedule.length; i++ ){
+      realSchedule[i]["schedule"].forEach((slot){
+        // daySlots.forEach((slot){
+          convertedRealSchedule.add({
+            "day": i+1, 
+            "start_time": "${slot[0].hour}:${slot[0].minute}",
+            "end_time": "${slot[1].hour}:${slot[1].minute}",
+            });
+      // });
+      });
+    }
+
+    log.i("Schedule being sent here >>>>> $convertedRealSchedule");
+    WorkingHourApiService.sendSchedule(convertedRealSchedule, context, onSuccess: () {
       _showSuccessMessage();
       _navigateToProfilePage();
     });
@@ -113,6 +168,13 @@ class _WorkingHoursState extends State<WorkingHours> {
 
   @override
   Widget build(BuildContext context) {
+    //* Initialize the list with one WorkingHoursInput widget for each day
+    dayInputs = <Widget>[];
+    // dayInputs = days.map((weekday) => _buildWorkingHoursInput(weekday)).toList();
+    realSchedule.asMap().forEach((index, weekday) {
+      dayInputs.add(_buildWorkingHoursInput(index, weekday));
+    });
+
     return Scaffold(
       key: _scaffoldKey, //* Assign the GlobalKey to the Scaffold
       backgroundColor: Color(0xFFF7F9FC),
@@ -164,8 +226,11 @@ class _WorkingHoursState extends State<WorkingHours> {
                 ),
                 SizedBox(height: 20),
                 //* Render WorkingHoursInput widgets dynamically
-                Column(
-                  children: dayInputs,
+                Padding(
+                  padding: const EdgeInsets.only(left: 25),
+                  child: Column(
+                    children: dayInputs,
+                  ),
                 ),
                 SizedBox(height: 20),
                 //* Buttons
