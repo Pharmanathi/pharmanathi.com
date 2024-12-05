13c6694baf908b9b9e60daa98145e8527b40c177
Nehemie BALUKIDI <alfred.nehemie@gmail.com>
2024-12-05 14:49:12 +0200

    fix: delete old and broken  folder

--- mobile-patient/android/app/src/main/kotlin/com/pharmanathi/patient/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.pharmanathi.patient
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
--- mobile-patient/lib/model/calender_events.dart
+++
@@ -1,6 +0,0 @@
-class Event {
-  final String eventName;
-  final List<List<String>> timeSlots;
-
-  Event(this.eventName, this.timeSlots);
-}
