6c0cb9afdde8370a8e27d4bde2f62b4cad013afe
Nehemie BALUKIDI <alfred.nehemie@gmail.com>
2024-12-30 00:58:05 +0200

    ci(e2e, patient): adds init integration test and CI server setup

---
+++ mobile_patient/ios/RunnerUITests/RunnerUITests.m
@@ -0,0 +1,11 @@
+//
+//  RunnerUITests.m
+//  RunnerUITests
+//
+//  Created by Néhémie Balukidi on 2024/12/30.
+//
+@import XCTest;
+@import patrol;
+@import ObjectiveC.runtime;
+
+PATROL_INTEGRATION_TEST_IOS_RUNNER(RunnerUITests)
