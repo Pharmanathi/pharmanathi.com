e4b34410daad2b4aaf7116a8d8f522dc7400f084
Nehemie BALUKIDI <alfred.nehemie@gmail.com>
2024-12-31 21:51:37 +0200

    fix: typography sizing and refactoring

--- mobile_mhp/lib/views/widgets/buttons.dart
+++ mobile_mhp/lib/views/widgets/buttons.dart
@@ -1,7 +1,9 @@
 // ignore_for_file: prefer_const_constructors
 
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:google_fonts/google_fonts.dart';
+import 'package:pharma_nathi/config/color_const.dart';
 
 class MyButtonWidgets {
   final String? buttonTextPrimary;
@@ -42,8 +44,9 @@ class MyButtonWidgets {
     bool showSecondary = true,
   }) {
     final primaryButtonStyle = ElevatedButton.styleFrom(
-      backgroundColor: Color(0xFF6F7ED7), // Background color for primary button
-      minimumSize: Size(320, 45),
+      backgroundColor: Color.fromARGB(
+          255, 132, 215, 111), // Background color for primary button
+      minimumSize: Size(360, 45),
       elevation: 0,
       shape: RoundedRectangleBorder(
         borderRadius: BorderRadius.circular(2),
@@ -52,7 +55,7 @@ class MyButtonWidgets {
 
     final secondaryButtonStyle = ElevatedButton.styleFrom(
       backgroundColor: Colors.white, // Background color for secondary button
-      minimumSize: Size(320, 45),
+      minimumSize: Size(360, 45),
       elevation: 0,
       side: BorderSide(
         color: Color(0xFF6F7ED7),
@@ -103,5 +106,88 @@ class MyButtonWidgets {
         children: buttons,
       ),
     );
+  }
+}
+
+class PrimaryButton extends StatelessWidget {
+  const PrimaryButton({super.key, required this.text, required this.onPressed});
+
+  final String text;
+  final VoidCallback onPressed;
+
+  @override
+  Widget build(BuildContext context) {
+    return ElevatedButton(
+        onPressed: onPressed,
+        child: Text(text,
+            style: GoogleFonts.openSans(
+              color: Pallet.PRIMARY_COLOR,
+              fontSize: 16.sp,
+              fontWeight: FontWeight.bold,
+            )));
+  }
+}
+
+class SecondaryButton extends StatelessWidget {
+  const SecondaryButton(
+      {super.key, required this.text, required this.onPressed});
+
+  final String text;
+  final VoidCallback onPressed;
+
+  @override
+  Widget build(BuildContext context) {
+    return ElevatedButton(
+        onPressed: onPressed,
+        style: ElevatedButton.styleFrom(
+          backgroundColor:
+              Pallet.PURE_WHITE, // Background color for secondary button
+          minimumSize: Size(360, 45),
+          elevation: 0,
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(4), // No border radius
+          ),
+        ),
+        child: Text(text,
+            style: GoogleFonts.openSans(
+              color: Pallet.PRIMARY_COLOR,
+              fontSize: 16.sp,
+              fontWeight: FontWeight.bold,
+            )));
+  }
+}
+
+class PrimaryButtonOutline extends StatelessWidget {
+  const PrimaryButtonOutline(
+      {super.key, required this.text, required this.onPressed});
+
+  final String text;
+  final VoidCallback onPressed;
+
+  @override
+  Widget build(BuildContext context) {
+    return ElevatedButton(
+        onPressed: onPressed,
+        style: ElevatedButton.styleFrom(
+          backgroundColor:
+              Pallet.PRIMARY_COLOR, // Background color for secondary button
+          minimumSize: Size(360, 45),
+          elevation: 0,
+          side: BorderSide(
+            color: Pallet.PURE_WHITE,
+            width: 2,
+          ),
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(4), // No border radius
+          ),
+        ),
+        child: Text(
+          text,
+          style: GoogleFonts.openSans(
+            color: Pallet.PURE_WHITE,
+            fontSize: 16.sp,
+            fontWeight: FontWeight.bold,
+          ),
+        ));
   }
 }
