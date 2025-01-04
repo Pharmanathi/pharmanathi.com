d33a1ada737b4ad781f3d70aac5745ea61e94ba8
Nehemie BALUKIDI <alfred.nehemie@gmail.com>
2025-01-04 18:04:41 +0200

    fix(mhp): session with chief design continues
    - added and partially applied custom googlefont opensans
    
    - further fixes to code to UI to match figma design

--- mobile_mhp/lib/views/widgets/navigationbar.dart
+++ mobile_mhp/lib/views/widgets/navigationbar.dart
@@ -1,5 +1,6 @@
 // ignore_for_file: prefer_const_constructors, prefer_const_literals_to_create_immutables, use_key_in_widget_constructors, prefer_const_constructors_in_immutables
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 
 class CustomBottomNavigationBar extends StatelessWidget {
   final int currentIndex;
@@ -21,31 +22,34 @@ class CustomBottomNavigationBar extends StatelessWidget {
         onTap(index); // Call the provided callback to handle tap events
         switch (index) {
           case 0:
-            Navigator.pushNamed(context, '/home_page'); // Navigate to the Home screen
+            Navigator.pushNamed(
+                context, '/home_page'); // Navigate to the Home screen
             break;
           case 1:
-            Navigator.pushNamed(context, '/appointments'); // Navigate to the Appointments screen
+            Navigator.pushNamed(context,
+                '/appointments'); // Navigate to the Appointments screen
             break;
-            case 2:
-            Navigator.pushNamed(context, '/patient_list'); // Navigate to the Appointments screen
+          case 2:
+            Navigator.pushNamed(context,
+                '/patient_list'); // Navigate to the Appointments screen
             break;
-            case 3:
-            Navigator.pushNamed(context, '/profile'); // Navigate to the Appointments screen
+          case 3:
+            Navigator.pushNamed(
+                context, '/profile'); // Navigate to the Appointments screen
             break;
-            // case 4:
-            // Navigator.pushNamed(context, '/profile'); // Navigate to the Appointments screen
-            // break;
+          // case 4:
+          // Navigator.pushNamed(context, '/profile'); // Navigate to the Appointments screen
+          // break;
           // Add cases for other items if needed
         }
       },
       items: [
         BottomNavigationBarItem(
-
-          icon: Icon(Icons.home),
+          icon: Icon(Icons.home, size: 32.w),
           label: 'Home',
         ),
         BottomNavigationBarItem(
-          icon: Icon(Icons.calendar_month),
+          icon: Icon(Icons.calendar_month, size: 32.w),
           label: 'Appointment',
         ),
         // BottomNavigationBarItem(
@@ -53,11 +57,11 @@ class CustomBottomNavigationBar extends StatelessWidget {
         //   label: 'Walet',
         // ),
         BottomNavigationBarItem(
-          icon: Icon(Icons.group),
+          icon: Icon(Icons.group, size: 32.w),
           label: 'Patient',
         ),
         BottomNavigationBarItem(
-          icon: Icon(Icons.person),
+          icon: Icon(Icons.person, size: 32.w),
           label: 'Profile',
         ),
       ],
