3a96e8deca8a2d6fe97d61d5a6ab5a85a98fdbfc
Nehemie BALUKIDI <alfred.nehemie@gmail.com>
2025-02-03 10:52:18 +0200

    test: match existing logo

--- mobile_patient/integration_test/sign_in_test.dart
+++ mobile_patient/integration_test/sign_in_test.dart
@@ -18,21 +18,9 @@ void main() {
       // Verify the Pharma Nathi logo and text are present
       expect(
         find.image(
-            const AssetImage('assets/images/pharmanathi-patient-icon.png')),
+            const AssetImage('assets/images/pharmanathi-stack-logoblue.png')),
         findsOneWidget,
         reason: 'Pharma Nathi logo should be visible',
-      );
-
-      expect(
-        find.text('PHARMA'),
-        findsOneWidget,
-        reason: 'PHARMA text should be visible',
-      );
-
-      expect(
-        find.text('NATHI'),
-        findsOneWidget,
-        reason: 'NATHI text should be visible',
       );
 
       // Verify the Google Sign-In button is present
