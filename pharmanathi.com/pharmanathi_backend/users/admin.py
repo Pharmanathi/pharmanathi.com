7beee6b7f0569c2f74f56a029ce91a8ba54643b0
Nehemie BALUKIDI <alfred.nehemie@gmail.com>
2025-02-13 15:40:43 +0200

    chore: static analysis

--- pharmanathi.com/pharmanathi_backend/users/admin.py
+++ pharmanathi.com/pharmanathi_backend/users/admin.py
@@ -3,6 +3,7 @@ from django.contrib import admin
 from django.contrib.auth import admin as auth_admin
 from django.contrib.auth import decorators, get_user_model
 from django.utils.translation import gettext_lazy as _
+
 from pharmanathi_backend.users.forms import UserAdminChangeForm, UserAdminCreationForm
 
 from . import models
